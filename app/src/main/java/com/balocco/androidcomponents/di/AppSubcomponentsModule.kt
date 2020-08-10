4d194e2142744f044aeb11fdc175d8f22ff39720
Alessandro Balocco <alex.balo@gmail.com>
2020-08-10 12:11:05 +0200

    Add component for DetailsActivity

--- app/src/main/java/com/balocco/androidcomponents/di/AppSubcomponentsModule.kt
+++ app/src/main/java/com/balocco/androidcomponents/di/AppSubcomponentsModule.kt
@@ -1,10 +1,12 @@
 package com.balocco.androidcomponents.di
 
+import com.balocco.androidcomponents.feature.detail.di.DetailComponent
 import com.balocco.androidcomponents.feature.toprated.di.TopRatedComponent
 import dagger.Module
 
 @Module(
     subcomponents = [
-        TopRatedComponent::class]
+        TopRatedComponent::class,
+        DetailComponent::class]
 )
 class AppSubcomponentsModule
